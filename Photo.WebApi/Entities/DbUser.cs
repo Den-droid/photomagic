ebb9b5a334de78c8941ebaca50f2d695abf0b687
Den-droid <amerscan8@gmail.com>
2020-11-14 17:01:00 +0200

    Initialize. Add account,db,img,profile

---
+++ Photo.WebApi/Entities/DbUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photo.WebApi.Entities
+{
+    public class DbUser : IdentityUser<long>
+    {
+        public virtual ICollection<DbUserRole> UserRoles { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
+    }
+}
---
+++ Photo.WebApi/Models/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photo.WebApi.Models
+{
+    public class UserViewModel
+    {
+        public class UserLoginViewModel
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class UserRegisterViewModel
+        {
+            public string Email { get; set; }
+            public string ImageBase64 { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
